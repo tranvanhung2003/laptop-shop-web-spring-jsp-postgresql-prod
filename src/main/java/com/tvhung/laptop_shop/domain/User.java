5e5f230a22c9260a9e16cb595e2229074ecc4b45
Trần Văn Hưng <tranvanhan29042007@gmail.com>
2026-07-31 14:17:43 +0700

    Fix database URL in .env.example, update .gitignore for VS Code, add launch and settings configurations, enhance User entity with JPA annotations, and change Hibernate ddl-auto to create-drop in application.properties

--- src/main/java/com/tvhung/laptop_shop/domain/User.java
+++ src/main/java/com/tvhung/laptop_shop/domain/User.java
@@ -1,13 +1,35 @@
 package com.tvhung.laptop_shop.domain;
 
-import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String email;
+
     private String password;
+
     private String fullName;
+
     private String address;
+
     private String phoneNumber;
 }
